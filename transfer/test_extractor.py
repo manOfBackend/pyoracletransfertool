16bc9ae15ac913a0c30b6b779d7ef36ed30039de
unknown <manofbackend@fasoo.com>
2021-01-29 18:00:15 +0900

    modify test case

--- transfer/test_extractor.py
+++ transfer/test_extractor.py
@@ -14,7 +14,7 @@ class Testextractor(TestCase):
 
 
     # @unittest.skip("skip")
-    def test_fetch(self):
+    def test_fetch_138MB_4000_1THREAD(self):
         col_name = "*"
         table_name = "adid_test"
         file_name = "output_adid_test"
@@ -24,3 +24,17 @@ class Testextractor(TestCase):
                   db_name=self.db_name, password=self.password, host_name=self.host_name,
                   data_dir_path=self.data_dir_path,
                   file_name=file_name)
+
+    def test_fetch_1GB_4000_1THREAD(self):
+        col_name = "*"
+        table_name = "adid_test_1GB"
+        file_name = "output_%s" % table_name
+        execute_query = """ select %s from %s """ % (col_name, table_name)
+        count_query = """ select count(*) from %s  """ % table_name
+        extractor(count_query=count_query, execute_query=execute_query, thread_count=1, fetch_size=1000,
+                  db_name=self.db_name, password=self.password, host_name=self.host_name,
+                  data_dir_path=self.data_dir_path,
+                  file_name=file_name)
+
+
+
